969f0bd9ade019611479134df98732a65134c8e0
Вовий <mackingo@mail.ru>
2026-02-27 16:14:12 +0300

    okr

---
+++ Desktop/index.js
@@ -0,0 +1,16 @@
+//1 задание 
+// ответ: B) filter
+
+//2 задание
+function quadro(){
+    for(i = 1; i <= 15; i++ )
+    {
+        if(i%2 !== 0)
+        {
+            console.log(i*i)
+        }
+    }
+    }
+
+quadro();
+
